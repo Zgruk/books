bb3a9b549785ce2790f9e789430d95a64d98d37f
Antoine <anton.pukhart@gmail.com>
2022-11-08 20:39:49 +0300

    Added docker-compose-prod.yml, changed .gitignore

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('travka/', admin.site.urls),
 
     # User registration
     # path('accounts/', include('django.contrib.auth.urls')),
